2504deb432064f7af790c0bae5ec22bf2b70e09a
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-03-11 16:41:14 -0400

    Page Links working

---
+++ js/main.js
@@ -0,0 +1,35 @@
+class TabLink {
+  constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.tab;
+    this.itemElement = document.querySelector(
+      `.content[data-tab='${this.data}']`
+    );
+    this.tabItem = new TabItem(this.itemElement);
+    this.element.addEventListener("click", () => this.select());
+  }
+
+  select() {
+    const linkTab = document.querySelectorAll(`.link`);
+    linkTab.forEach(function(linkTab) {
+      linkTab.classList.remove(`change`);
+    });
+    this.element.classList.toggle(`change`);
+    this.tabItem.select();
+  }
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    const items = document.querySelectorAll(`.content`);
+    items.forEach(items => items.classList.remove("change"));
+    this.element.classList.toggle("change");
+  }
+}
+
+let links = document.querySelectorAll(`.link`).forEach(link => new TabLink(link));
+console.log(links);
